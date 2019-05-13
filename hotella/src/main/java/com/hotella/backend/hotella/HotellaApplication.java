05d12221132d55785c72b66184a3311f6a5fd11a
Xavier Passos Rodrigues <xavi.passos@gmail.com>
2019-05-13 14:43:15 +0100

    started to mount the structure to the backend

---
+++ hotella/src/main/java/com/hotella/backend/hotella/HotellaApplication.java
@@ -0,0 +1,13 @@
+package com.hotella.backend.hotella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotellaApplication.class, args);
+	}
+
+}
