e96d2890a16f576c2f122d5d5a0fcdcd16d9394d
Xavier Passos Rodrigues <xavi.passos@gmail.com>
2019-05-21 17:43:05 +0100

    last change on project structure

--- hotella/src/main/java/com/hotella/backend/hotella/repository/HotelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hotella.backend.hotella.repository;
-
-import com.hotella.backend.hotella.model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-
-}
